a721f6d1985cdb5611bf7d101363030ca806672d
rcj1 <rachel.jarvi@gmail.com>
2023-03-04 10:50:11 -0500

    Add project files.

---
+++ WindowsFormsApp1/Form1.Designer.cs
@@ -0,0 +1,129 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.ColumnCount = 2;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 0);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 2;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(200, 100);
+            this.tableLayoutPanel1.TabIndex = 0;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Enabled = false;
+            this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox1.Location = new System.Drawing.Point(328, 0);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ReadOnly = true;
+            this.textBox1.Size = new System.Drawing.Size(100, 26);
+            this.textBox1.TabIndex = 1;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Enabled = false;
+            this.textBox2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox2.Location = new System.Drawing.Point(254, 404);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.ReadOnly = true;
+            this.textBox2.Size = new System.Drawing.Size(262, 26);
+            this.textBox2.TabIndex = 2;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.tableLayoutPanel1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+        private void Form1_Load()
+        {
+            this.tableLayoutPanel1.ColumnCount = 3;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 30);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 3;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size((int) (this.ClientSize.Width * 0.8), (int)(this.ClientSize.Height * 0.8));
+            this.tableLayoutPanel1.TabIndex = 0;
+
+            //this.textBox2.Text = string.Format("{0} is playing", symbols[x]);
+            this.textBox1.TextAlign = HorizontalAlignment.Center;
+            this.textBox2.TextAlign = HorizontalAlignment.Center;
+
+            for (int i = 0; i < 3; i++)
+            {
+                for (int j = 0; j < 3; j++)
+                {
+
+                    var button = new Button();
+                    //button.Text = string.Format("{0}{1}", i, j);
+                    button.Font = new System.Drawing.Font("Microsoft Sans Serif", 12);
+                    button.Name = string.Format("{0}{1}", i, j);
+                    button.Dock = DockStyle.Fill;
+                    button.Click += new System.EventHandler(this.buttonClick);
+                    this.tableLayoutPanel1.Controls.Add(button, j, i);
+                }
+            }
+        }
+
+
+        #endregion
+
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private TextBox textBox1;
+        private TextBox textBox2;
+    }
+}
+
---
+++ WindowsFormsApp1/Form1.cs
@@ -0,0 +1,144 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{   
+    public partial class Form1 : Form
+    {   
+        public Form1()
+        {
+            InitializeComponent();
+            Form1_Load();
+            Game_Start();
+
+        }
+
+        private void Game_Start()
+        {
+            this.xplaces = new List<string>();
+            this.oplaces = new List<string>();
+            this.x = r.Next(2);
+            //this.textBox1.Text = "";
+            foreach (Control ctrl in this.tableLayoutPanel1.Controls)
+            {
+                ctrl.Text = "";
+            }
+            this.textBox2.Text = string.Format("{0} is playing", symbols[x]);
+        }
+
+        private static Random r = new Random();
+        List<String> xplaces;
+        List<String> oplaces;
+        List<List<string>> winners = new List<List<string>>()
+            {
+                new List<string>()
+                {
+                    "00", "01", "02"
+                },
+                new List<string>()
+                {
+                    "10", "11", "12"
+                },
+                new List<string>()
+                {
+                    "20", "21", "22"
+                },
+                new List<string>()
+                {
+                    "00", "10", "20"
+                },
+                new List<string>()
+                {
+                    "01", "11", "21"
+                },
+                new List<string>()
+                {
+                    "02", "12", "22"
+                },
+                new List<string>()
+                {
+                    "00", "11", "22"
+                },
+                new List<string>()
+                {
+                    "02", "11", "20"
+                },
+            };
+        int x;
+        String[] symbols = { "O", "X" };
+
+        protected void add_place(object sender, int x)
+        {
+            string name = ((Button)sender).Name;
+            if (x == 0)
+            {
+                xplaces.Add(name);
+            }
+            else
+            {
+                oplaces.Add(name);
+            }
+        }
+
+
+        protected bool check_for_win(int x)
+        {
+            List<string> myl;
+            if (x == 0)
+            {
+                myl = xplaces;
+            }
+            else
+            {
+                myl = oplaces;
+            }
+
+            foreach(List<string> l in winners)
+            {
+                if (myl.Contains(l[0]) && myl.Contains(l[1]) && myl.Contains(l[2])) {
+                    
+                    return true;
+                }
+            }
+            return false;
+        }
+
+        protected void letter_change(int newx)
+        {
+            x = newx;
+            this.textBox2.Text = string.Format("{0} is playing", symbols[newx]);
+        }
+
+        private void buttonClick(object sender, EventArgs e)
+        {
+            (sender as Button).Text = symbols[x];
+            add_place(sender, x);
+            var temp = x;
+            if (x == 0)
+            {
+                letter_change(1);
+                this.textBox1.Text = "";
+            }
+            else
+            {
+                letter_change(0);
+                this.textBox1.Text = "";
+            }
+
+            if (check_for_win(temp))
+            {
+                this.textBox1.Text = string.Format("{0} won", symbols[temp]);
+                System.Threading.Thread.Sleep(200);
+                Game_Start();
+            }
+        }
+    }
+}
